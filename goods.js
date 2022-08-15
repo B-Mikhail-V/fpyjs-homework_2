f49e4980c6811761c4c9deb3182979e6032da379
Mikail Bakulin <bakulin.mv@gmail.com>
2022-08-15 14:25:50 +0300

    Corr_goods_4

--- goods.js
+++ goods.js
@@ -1,4 +1,4 @@
-const goods = {
+const goods = {    
     1: {
         id: 1,
         name: 'Шляпа',
@@ -21,24 +21,24 @@ const goods = {
         description: 'Классика',
         sizes: [48, 50, 52],
         price: 3800,
-        available: 'нет',
+        available: 'да',
     },
     4: {
-        id: 6,
+        id: 4,
         name: 'Ботинки',
         description: 'Синие на тонкой подошве',
         sizes: [38, 39, 40, 41, 42, 44, 46],
         price: 4900,
-        available: 'под заказ',
+        available: 'нет',
     },
     5: {
-        id: 8,
+        id: 5,
         name: 'Рубашка',
         description: 'В клеточку',
         sizes: [39, 40, 42],
         price: 2300,
         available: 'да',
-    },
+    }
 }
 
 const basket = [
@@ -46,86 +46,65 @@ const basket = [
         good: 1,
         size: 58,
         qty: 2,  
-    }
+    },
+    {
+        good: 3,
+        size: 50,
+        qty: 1,  
+    },
 ]
-// const basket = []
-
-// console.log(goods[4].sizes[basket[0].size])
-// console.log(Object.keys(goods).length)
-
-const goodsAdd = {
-        id: 15,
-        name: 'Кроссовки',
-        description: 'Для бега',
-        sizes: [40, 42, 44],
-        price: 3200,
-        available: 'да',
-    }
-
-// console.log(basket)
-
-// function addGood (goodsAdd) {
-//     const idAdd = Object.keys(goods).length + 1;
-//     goods[idAdd] = goodsAdd;
-// }
-
-// function deleteGood (idDelete) {
-//     delete goods[idDelete];
-// }
-
-// addGood(goodsAdd);
-
-// deleteGood();
-
-function addGoodBasket(goodId, goodSizeId, goodQty) {
-    
+// создание массива ссылок 
+goodsIndex = []
+for (let i = 1; i <= Object.keys(goods).length; i++) {
+    goodsIndex.push(goods[i])
 }
-console.log(basket)  
 
-// goodId = 3
-// goodSizeId = 52
-// goodQty = 2
-// basket.length = 0;
-console.log(basket) 
-
-function createBasketData(goodId, goodSizeId, goodQty) {
-    
-    for (let i = 1; i <= Object.keys(goods).length; i++) {
-        if (goods[i].id == goodId) {
-            // console.log(goods[i].sizes[goodSizeId])
-            basketData = {good: goodId, size: goodSizeId, qty: goodQty}
-            // basket.push(basketData)
-            // console.log(basket)   
-        } else {
-            return;
-            
-        }
-    }
-
+// проверка, есть ли указанный товар в корзине
+function isAlreadyInBasket(goodId, goodSize) {
     for (let i = 0; i < Object.keys(basket).length; i++) {
-        // console.log(basket[i])
-        if (basket[i].id == basketData.good || basket[i].size == basketData.size) {
-            // console.log("OK")
-            basket[i].qty += goodQty;
-            break;
-        } else {
-            basket.push(basketData);
-            break;
-    
-        }
+        if (basket[i].good == goodId && basket[i].size == goodSize) {
+            return i;
+        } /*else {
+            return false;
+        }*/
     }
-
 }
         
 
+// проверка, доступен ли товар для добавления в корзину
+function isGoodAvailable(goodId, goodSize) {
+    for (let i = 0; i < Object.keys(goodsIndex).length; i++) {
+        if (goodsIndex[i].id == goodId && goodsIndex[i].sizes.includes(goodSize) && goodsIndex[i].available == 'да') {
+            return true;
+        } /*else {
+            return false;
+        }*/
+    }
+}
+    
+        
+// добавление товара в корзину; на входе id, размер и количество товара. 
+function addGoodBasket(goodId, goodSize, goodQty) {
+    if (isAlreadyInBasket(goodId, goodSize) !== undefined && isGoodAvailable(goodId, goodSize)) {
+        basket[isAlreadyInBasket(goodId, goodSize)].qty += goodQty
+        console.log ("Указанный товар уже находился в корзине, количество изменено на ", basket[isAlreadyInBasket(goodId, goodSize)].qty)
+        return;
+    } else if (isAlreadyInBasket(goodId, goodSize) == undefined && isGoodAvailable(goodId, goodSize)) {
+        basket.push({good: goodId, size: goodSize, qty: goodQty})
+        console.log ("Указанный товар добавлен в корзину")
+        return
+    }   else if (isGoodAvailable(goodId, goodSize) == undefined) {
+        console.log ("Указанный товар недоступен, состав корзины не изменился")
+        return
+    }
+
+}
+
+
+        
+addGoodBasket(2, 44, 3)
 
 
 
 
-
-createBasketData(1, 58, 2)
-// basket.length = 0;
-// basket.push({good: 1, size: 22, qty: 1})
 console.log(basket)  
-
-// console.log(basket)
